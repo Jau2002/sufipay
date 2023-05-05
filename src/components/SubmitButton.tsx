0846c283813bd2bf14526eb445ac9fd2036e9231
Jau2002 <idamendjaume@gmail.com>
2023-05-05 12:10:49 -0500

    style: button display requeriments

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -1,11 +1,19 @@
 import type { FC } from 'react';
+import style from '../css/submit.module.css';
 
 interface SubmitButtonComponentProps {
 	children: string;
 }
 
 const SubmitButton: FC<SubmitButtonComponentProps> = ({ children }) => {
-	return <button type='submit'>{children}</button>;
+	return (
+		<button
+			type='submit'
+			className={style.button}
+		>
+			{children}
+		</button>
+	);
 };
 
 export default SubmitButton;
