8fbeac635d612f1dac8ac75ae8fbf5e61a70cde4
Jau2002 <idamendjaume@gmail.com>
2023-05-05 12:16:03 -0500

    style: component inputs for login

--- src/components/Access.tsx
+++ src/components/Access.tsx
@@ -1,4 +1,5 @@
 import type { FC } from 'react';
+import style from '../css/access.module.css';
 
 interface AccessComponentProps {
 	name: string;
@@ -7,9 +8,15 @@ interface AccessComponentProps {
 
 const Access: FC<AccessComponentProps> = ({ name, type = 'text' }) => {
 	return (
-		<section>
-			<label htmlFor={name}>{name}</label>
+		<section className={style.section}>
+			<label
+				htmlFor={name}
+				className={style.sectionLabel}
+			>
+				{name}
+			</label>
 			<input
+				className={style.sectionInput}
 				type={type}
 				name={name}
 				id={name}
